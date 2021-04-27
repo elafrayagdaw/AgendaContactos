86a6e507bb33655aace06040d1b9158a885559f2
uperezcdaw1 <uperezcdaw1@gmail.com>
2021-04-27 20:26:03 +0200

    AgendaIO acabada

--- src/agenda/io/AgendaIO.java
+++ src/agenda/io/AgendaIO.java
@@ -1,5 +1,4 @@
 package agenda.io;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -20,30 +19,36 @@ public class AgendaIO {
 	 * 
 	 */
 	public static int importar(AgendaContactos agenda, String ruta) {
-		int contador = 0;
+		int contador = 0;  
 		int suma = 0;
 		BufferedReader entrada = null;
 		try {
 			entrada = new BufferedReader(new FileReader(ruta));
 			String linea = entrada.readLine();
 			while (linea != null) {
-				agenda.añadirContacto(parsearLinea(linea));
+				Contacto contacto = parsearLinea(linea);
+				if(contacto != null) {
+				agenda.añadirContacto(contacto);}
+				else {linea = entrada.readLine();}			
+				linea = entrada.readLine();
+			}
+			if(linea == null) {
 				linea = entrada.readLine();
 			}
 		} catch (IOException e) {
-			suma += 1;
+			suma++;
 			System.out.println("Error al leer " + ruta);
 		} catch (NullPointerException e) {
-			System.out.println("Campo es " + e.getMessage());
+			System.out.println("Documento vacío");
 		} finally {
 			if (entrada != null) {
 				try {
 					entrada.close();
 				} catch (NullPointerException e) {
-					suma += 1;
+					suma++;
 					System.out.println(e.getMessage());
 				} catch (IOException e) {
-					suma += 1;
+					suma++;
 					System.out.println(e.getMessage());
 				}
 			}
@@ -59,32 +64,29 @@ public class AgendaIO {
 	 * Contacto.
 	 * 
 	 */
-	private static Contacto parsearLinea(String linea) {
-		Contacto con = null;
-		Profesional prof;
-		Personal per;
-
+	private static Contacto parsearLinea(String linea){
 		String[] separacion = linea.split(",");
-		for (int i = 0; i < separacion.length; i++) {
+		for(int i = 0; i < separacion.length; i++) {
 			separacion[i] = separacion[i].trim();
 		}
-		String nombre;
-		String apellidos;
+		String nombre ; 
+		String apellidos; 
 		String telefono;
 		String email;
 		int numClase = Integer.parseInt(separacion[0]);
 		Relacion relac = Relacion.HIJO;
 		String atributoNoComun1;
-
+		
 		try {
-			if (numClase == 1) {
-				nombre = separacion[1];
-				apellidos = separacion[2];
+			if(numClase == 1) {
+				nombre = separacion[1]; 
+				apellidos = separacion[2]; 
 				telefono = separacion[3];
-				email = separacion[4];
+				email = separacion[4];;
 				atributoNoComun1 = separacion[5];
-				prof = new Profesional(nombre, apellidos, telefono, email, atributoNoComun1);
-				con = (Contacto) prof;
+				Profesional prof = new Profesional(nombre, apellidos, telefono, email, atributoNoComun1);
+				Contacto con = (Contacto) prof;
+				return con;
 			}
 		} catch (NullPointerException e) {
 			System.out.println("Hay campos nulos en el contacto");
@@ -92,16 +94,16 @@ public class AgendaIO {
 			System.out.println("Error en el formato numérico de un campo del contacto");
 		} catch (Exception e) {
 			System.out.println("Error con un campo del contacto");
-		} 
-
+		}
+		
 		try {
-			if (numClase == 2) {
-				nombre = separacion[1];
-				apellidos = separacion[2];
+			if(numClase == 2) {
+				nombre = separacion[1]; 
+				apellidos = separacion[2]; 
 				telefono = separacion[3];
 				email = separacion[4];
 				atributoNoComun1 = separacion[5];
-
+				
 				separacion[6].toUpperCase();
 				switch (separacion[6].toUpperCase()) {
 				case "PADRE":
@@ -123,9 +125,10 @@ public class AgendaIO {
 					relac = relac.PAREJA;
 					break;
 				}
-
-				per = new Personal(nombre, apellidos, telefono, email, atributoNoComun1, relac);
-				con = (Contacto) per;
+				
+				Personal prof = new Personal(nombre, apellidos, telefono, email, atributoNoComun1, relac);
+				Contacto con = (Contacto) prof;
+				return con;
 			}
 		} catch (NullPointerException e) {
 			System.out.println("Hay campos nulos en el contacto");
@@ -133,7 +136,10 @@ public class AgendaIO {
 			System.out.println("Error en el formato numérico de un campo del contacto");
 		} catch (Exception e) {
 			System.out.println("Error con un campo del contacto");
-		} 
-		return con;
+		}
+		
+		return null;
 	}
+
 }
+
